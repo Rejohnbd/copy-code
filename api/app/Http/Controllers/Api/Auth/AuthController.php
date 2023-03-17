373887646cc6db416f64f5398628829f0341ed3b
rejohnbd <rejohn.cse@gmail.com>
2023-03-17 16:03:42 +0600

    Add Traits for Response

--- api/app/Http/Controllers/Api/Auth/AuthController.php
+++ api/app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,51 +3,65 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\Auth\AuthRepository;
+use App\Traits\ResponseTrait;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\PersonalAccessTokenResult;
+use Exception;
+use Illuminate\Http\Response as Res;
 
 class AuthController extends Controller
 {
+    use ResponseTrait;
 
     public function login(Request $request)
     {
         $validator = validator(
             $request->all(),
             [
-                'email'     => 'required|email',
-                'password'  => 'required|string'
+                'email'     => 'required|string|email|max:255',
+                'password'  => 'required|string|min:8'
             ],
             [
-                'email.required'      => 'Please Give User Email',
-                'password.required'      => 'Please Give User Password',
+                'email.required'    => 'Please Give User Email',
+                'email.string'      => 'Provide Vaild Email Address',
+                'email.email'       => 'Provide Vaild Email Address',
+                'email.max'         => 'Provide Vaild Email Address',
+                'password.required' => 'Please Give User Password',
+                'password.string'   => 'Provide Valid User Password',
+                'password.min'      => 'Password Length at Least 8 Charecters',
             ]
         );
 
-        if ($validator->fails()) {
-            return response()->json([
-                'status'    => 400,
-                'message'   => $validator->getMessageBag()->first(),
-                'errors'    => $validator->getMessageBag()
-            ]);
-        }
-
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $user = Auth::user();
-            $accessToken = $user->createToken('authToken')->accessToken;
-
-            return response()->json([
-                'status'        => 200,
-                'message'       => 'Logged in successfully',
-                'user'          => $user,
-                'access_token'  => $accessToken
-            ]);
-        } else {
-            return response()->json([
-                'status'    => 401,
-                'message'   => 'Sorry Invalid Email and Password',
-                'errors'    => null
-            ]);
-        }
+        if ($validator->fails()) :
+            return $this->responseValidationError(Res::HTTP_BAD_REQUEST, $validator->getMessageBag()->first(), $validator->getMessageBag());
+        else :
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) :
+                $user = Auth::user();
+                // $tokenInstance = $this->getAuthToken($user);
+                // $expiresAt = $this->getExpiresAt($tokenInstance);
+                $tokenInstance = $user->createToken('authToken');
+                $expiresAt = Carbon::parse($tokenInstance->token->expires_at)->toDateTimeString();
+                try {
+                    // throw new Exception("Error Proceesing Request", 1);
+                    return $this->responseWithToken(Res::HTTP_OK, $tokenInstance->accessToken, $user, $expiresAt);
+                } catch (Exception $e) {
+                    return $this->resposeWithExpectationFailed(Res::HTTP_EXPECTATION_FAILED, $e->getMessage());
+                }
+            else :
+                return $this->responseError(Res::HTTP_UNAUTHORIZED, 'Sorry Invalid Email and Password');
+            endif;
+        endif;
     }
+
+    // public function getAuthToken(User $user)
+    // {
+    //     return $user->createToken('authToken');
+    // }
+
+    // public function getExpiresAt(PersonalAccessTokenResult $tokenInstance)
+    // {
+    //     return Carbon::parse($tokenInstance->token->expires_at)->toDateTimeString();
+    // }
 }
---
+++ api/app/Traits/ResponseTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+trait ResponseTrait
+{
+    public function responseValidationError($responseCode, $message, $errors = [])
+    {
+        return response()->json([
+            'status'    => false,
+            'message'   => $message,
+            'errors'    => $errors
+        ], $responseCode);
+    }
+
+    public function responseError($responseCode, $message)
+    {
+        return response()->json([
+            'status'    => false,
+            'message'   => $message,
+        ], $responseCode);
+    }
+
+    public function resposeWithExpectationFailed($responseCode, $message = 'Something went wrong')
+    {
+        return response()->json([
+            'status'        => false,
+            'message'       => $message,
+        ], $responseCode);
+    }
+
+    public function responseWithToken($responseCode, $token, $user, $expiresAt)
+    {
+        return response()->json([
+            'status'        => true,
+            'message'       => 'Logged in successfully',
+            'user'          => $user,
+            'token'         => $token,
+            'expires_at'    => $expiresAt
+        ], $responseCode);
+    }
+}
